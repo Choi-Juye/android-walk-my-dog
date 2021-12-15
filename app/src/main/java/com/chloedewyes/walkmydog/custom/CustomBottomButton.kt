67cd6a6919042ebd44833329af244fd5ece27544
chloedewyes <chloedewyes@gmail.com>
2021-12-15 19:04:06 +0900

    feat: 중복 사용되는 Bottom Button을 Custom View로 만들었습니다.

---
+++ app/src/main/java/com/chloedewyes/walkmydog/custom/CustomBottomButton.kt
@@ -0,0 +1,48 @@
+package com.chloedewyes.walkmydog.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.chloedewyes.walkmydog.R
+
+class CustomBottomButton(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+
+    private var customText: TextView
+
+    init {
+        val v = View.inflate(context, R.layout.custom_bottom_btn, this)
+        customText = v.findViewById(R.id.customText)
+
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.CustomBottomButton,
+            0,0
+        ).apply {
+            try {
+                setText(getString(R.styleable.CustomBottomButton_customText))
+                setTextColor(getColor(R.styleable.CustomBottomButton_customTextColor, 0))
+            } finally {
+                recycle()
+            }
+
+        }
+    }
+
+    fun setText(text: String?) {
+        customText.text = text
+        onRefresh()
+    }
+
+    fun setTextColor(color: Int){
+        customText.setTextColor(color)
+    }
+
+    private fun onRefresh() {
+        invalidate()
+        requestLayout()
+    }
+}
+
+
